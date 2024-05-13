53efdad9d40060d0b1ac650e3e2ac14bb270e117
armandoschianodicola <armando.schianodicola@gmail.com>
2024-05-13 16:53:02 +0200

    upd utils

---
+++ utils/path_utils.py
@@ -0,0 +1,10 @@
+import git
+
+class PathUtils:
+
+    @staticmethod
+    def get_git_root(path):
+
+        git_repo = git.Repo(path, search_parent_directories=True)
+        git_root = git_repo.git.rev_parse("--show-toplevel")
+        return git_root
